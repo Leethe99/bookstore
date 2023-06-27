a5bfb9cc23ef8747f8900e21088ea307a60ca6ad
Rafael Lee <leethe99@gmail.com>
2023-06-27 12:37:15 -0600

    added link to individual book pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,10 @@ class BooksController < ApplicationController
     def index
         @books = BookInventory.all
     end
+    
+    def show
+        @book = BookInventory.find(params[:id ])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "books/:id", to: "books#show "
+  get "books/:id", to: "books#show", as: :book 
   # Defines the root path route ("/")
   root "books#index"
 end
