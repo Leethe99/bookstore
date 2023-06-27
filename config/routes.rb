959ee4f8b20473e3223c687d494e2e3e951ae2b4
Rafael Lee <leethe99@gmail.com>
2023-06-27 12:04:04 -0600

    books now shown in index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "books/:id", to: "books#show "
   # Defines the root path route ("/")
   root "books#index"
 end
