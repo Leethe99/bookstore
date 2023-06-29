b4d66459600688dd816810427c7dc976c8269d6f
Rafael Lee <leethe99@gmail.com>
2023-06-29 17:29:47 -0600

    added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "books", to: "books#index"
   get "books/new", to: "books#new", as: :new_book
